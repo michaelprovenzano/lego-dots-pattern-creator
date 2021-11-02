27093f9e8e045f8b9d1b6c31e3d214d4ec7599b2
Michael Provenzano <mikepmusic@gmail.com>
2021-11-02 07:24:14 -0400

    Remove unecessary React import

--- src/components/KeyboardShortcuts/KeyboardShortcuts.component.jsx
+++ src/components/KeyboardShortcuts/KeyboardShortcuts.component.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { setEditor } from '../../redux/editor/editor.actions';
 
