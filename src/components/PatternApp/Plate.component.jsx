26225f785dad195f6618105ccc4268570f3d5f6a
Michael Provenzano <mikepmusic@gmail.com>
2021-11-02 07:38:55 -0400

    Catch errors if no renderer is available

--- src/components/PatternApp/Plate.component.jsx
+++ src/components/PatternApp/Plate.component.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import * as PIXI from 'pixi.js';
 import { rgbToHex } from '../../logic/utils';
 import { connect } from 'react-redux';
 
-const Plate = ({ pattern, viewport, generatorSettings, location, app }) => {
+const Plate = ({ pattern, viewport, editor, generatorSettings, location, app }) => {
   useEffect(() => {
+    if (!app.renderer) return;
     let { plateColor, width, height } = pattern;
     const studSize = generatorSettings.studSize;
     let [plateWidth, plateHeight] = [width * studSize, height * studSize];
@@ -14,6 +15,7 @@ const Plate = ({ pattern, viewport, generatorSettings, location, app }) => {
 
     var plate = new PIXI.Graphics();
     const { red, green, blue } = plateColor.rgb;
+    // eslint-disable-next-line
     plate.beginFill(eval(`0x${rgbToHex(red, green, blue)}`));
     plate.drawRect(0, 0, plateWidth, plateHeight);
     plate.endFill();
