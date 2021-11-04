f6f2179d5af4dabc5f1901f79b6c51ebd234f752
Michael Provenzano <mikepmusic@gmail.com>
2021-11-04 05:08:12 -0400

    Switch to HashRouter for the deployment on GitHub to work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import SettingsPage from './pages/SettingsPage/SettingsPage.component';
 import HomePage from './pages/HomePage/HomePage.component';
 
