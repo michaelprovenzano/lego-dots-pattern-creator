de89d07efa9f949a67d022d6a43396fbaf3d5326
Michael Provenzano <mikepmusic@gmail.com>
2021-11-04 06:15:09 -0400

    Bugfix: Range Input was not passing value to <input>

--- src/components/InputRange/InputRange.component.jsx
+++ src/components/InputRange/InputRange.component.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import './InputRange.styles.scss';
 
-const InputRange = ({ id, name, min, max, label, labelBefore, labelAfter, onChange }) => {
+const InputRange = ({
+  id,
+  name,
+  min,
+  max,
+  label,
+  labelBefore,
+  labelAfter,
+  onChange,
+  ...otherProps
+}) => {
   return (
     <div className='input-range'>
       <label htmlFor={id}>{label}</label>
       <div className='input-range__slider'>
         <div className='input-range__label-before'>{labelBefore}</div>
-        <input type='range' name={name} id={id} min={min} max={max} onChange={onChange} />
+        <input
+          type='range'
+          name={name}
+          id={id}
+          min={min}
+          max={max}
+          onChange={onChange}
+          {...otherProps}
+        />
         <div className='input-range__label-after'>{labelAfter}</div>
       </div>
     </div>
