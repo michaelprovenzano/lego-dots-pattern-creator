534299755b7ef5a18efc2c0cca1f0d986c397814
Michael Provenzano <mikepmusic@gmail.com>
2021-11-05 07:32:23 -0400

    Update app logo

--- src/components/AppTitle/AppTitle.component.jsx
+++ src/components/AppTitle/AppTitle.component.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './AppTitle.styles.scss';
 
+import { ReactComponent as Logo } from '../../images/logo.svg';
+
 const AppTitle = () => {
-  return <div className='app-title'>DOTS Pattern Creator</div>;
+  return (
+    <div className='app-title'>
+      <Logo height={80} />
+    </div>
+  );
 };
 
 export default AppTitle;
